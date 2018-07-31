cfdf3f5d6d6cfe15151ce3f7cae19c5110367c4d
cacheclear <contact@dennisking.de>
2018-07-31 17:27:26 +0200

    changes registration of repositories in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\ServiceProvider;
+use Elasticsearch\Client;
 use App\Articles\ArticlesRepository;
+use Illuminate\Support\ServiceProvider;
 use App\Articles\EloquentArticlesRepository;
+use App\Articles\ElasticsearchArticlesRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,8 +28,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(ArticlesRepository::class, function () {
-            return new EloquentArticlesRepository();
+        $this->app->singleton(ArticlesRepository::class, function($app) {
+            // This is useful in case we want to turn-off our
+            // search cluster or when deploying the search
+            // to a live, running application at first.
+            if (!config('services.search.enabled')) {
+                return new EloquentArticlesRepository();
+            }
+
+            return new ElasticsearchArticlesRepository(
+                $app->make(Client::class)
+            );
         });
     }
 }
