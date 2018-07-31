48d36ae57c77a7a3738321773215cef74dd014ea
cacheclear <contact@dennisking.de>
2018-07-31 16:24:03 +0200

    adds article seeder

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('articles')->truncate();
+        factory(App\Article::class, 50)->create();
+    }
+}
