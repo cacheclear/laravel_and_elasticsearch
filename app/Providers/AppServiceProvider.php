11fd571488d9ba51c09b7f4c20c7bf84f818144e
cacheclear <contact@dennisking.de>
2018-07-31 18:05:17 +0200

    fixes repo creating in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Articles\ArticlesRepository;
+use App\Articles\ElasticsearchArticlesRepository;
+use App\Articles\EloquentArticlesRepository;
 use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
 use Illuminate\Support\Facades\Schema;
@@ -27,7 +30,16 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(ArticlesRepository::class, function($app) {
+            // This is useful in case we want to turn-off our
+            // search cluster or when deploying the search
+            // to a live, running application at first.
+            if (!config('services.search.enabled')) {
+                return new EloquentArticlesRepository();
+            }
 
+            return new ElasticsearchArticlesRepository(
+                $app->make(Client::class)
+            );
         });
 
         $this->bindSearchClient();
